caa8309ed0f459dda5a593635592773c7c839fa0
codeylee05 <mlefa5002@gmail.com>
2025-10-15 17:11:51 +0200

    .

--- BeyonceVotingSite/settings.py
+++ BeyonceVotingSite/settings.py
@@ -29,13 +29,13 @@ if DEBUG:
     }
 else:
     DATABASES = {
-        "default": dj_database_url.config(
-            default=os.getenv("DATABASE_URL"),
-            conn_max_age=600,
-            ssl_require=True,
-        )
-    }
-
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+    
 
 INSTALLED_APPS = [
     'django.contrib.admin',
