fbc1cf24b3d0eeda8ed2607ea0f90e99b3574451
codeylee05 <mlefa5002@gmail.com>
2025-10-15 09:16:08 +0200

    Update settings.py

--- BeyonceVotingSite/settings.py
+++ BeyonceVotingSite/settings.py
@@ -7,6 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Ensure the folder exists 
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
