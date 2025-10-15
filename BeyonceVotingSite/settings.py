2226108085b2c97d1435c95ba7fb14b0b1f0fb5e
codeylee05 <mlefa5002@gmail.com>
2025-10-15 15:41:48 +0200

    Update settings.py

--- BeyonceVotingSite/settings.py
+++ BeyonceVotingSite/settings.py
@@ -64,7 +64,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 os.makedirs(STATIC_ROOT, exist_ok=True)  # Ensure folder exists
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+'''STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' '''
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core', 'static')]
 
