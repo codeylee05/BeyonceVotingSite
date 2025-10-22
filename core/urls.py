a1bd8235a328a8ba38f77af01644651b50001eb1
codeylee05 <mlefa5002@gmail.com>
2025-10-22 07:45:10 +0200

    Feature: Password reset (not complete)

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
 
@@ -17,6 +19,29 @@ urlpatterns = [
     path("voteforalbum/", views.album_voting, name="album_voting"),
 
     path("share/", views.referral, name="referral"),
+
+    # Auth-views for password reset
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='core/password_reset.html',
+             email_template_name='registration/password_reset_email.html'  # the email sent
+         ),
+         name='password_reset'),
+
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='core/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='core/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='core/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 
 
