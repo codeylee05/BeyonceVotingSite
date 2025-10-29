42d6507078fd8758cb39e182357e051860abc04e
codeylee05 <mlefa5002@gmail.com>
2025-10-29 12:36:14 +0200

    Custom admin for Votes display

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
-from .models import Album, Vote, Profile
 from django.contrib import admin
 from .models import Lobby, Profile, Album, Vote
+from django.db.models import Count
+from django.template.response import TemplateResponse
+from django.urls import path
 
 
 @admin.register(Lobby)
@@ -37,7 +39,38 @@ class AlbumAdmin(admin.ModelAdmin):
 
 @admin.register(Vote)
 class VoteAdmin(admin.ModelAdmin):
+
     list_display = ('user', 'album', 'date')
     list_filter = ('date', 'album')
     search_fields = ('user__username', 'album__title')
     ordering = ('-date',)
+    change_list_template = "admin/votes_by_album.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                'by-album/',
+                self.admin_site.admin_view(self.votes_by_album_view),
+                name='votes_by_album',
+            ),
+        ]
+        return custom_urls + urls
+
+    def votes_by_album_view(self, request):
+        albums = Album.objects.all().order_by('title')
+        data = []
+        for album in albums:
+            votes = Vote.objects.filter(album=album).select_related('user')
+            data.append({
+                'album': album,
+                'vote_count': votes.count(),
+                'voters': [v.user.username for v in votes],
+            })
+
+        context = dict(
+            self.admin_site.each_context(request),
+            data=data,
+            title='Votes grouped by Album',
+        )
+        return TemplateResponse(request, "admin/votes_by_album.html", context)
